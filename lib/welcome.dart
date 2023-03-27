929e54edc3aa60aafd6249c3c7cec5d5506a3fcd
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-27 22:57:07 +0530

    Update welcome.dart

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class welcome extends StatefulWidget {
+  const welcome({Key? key}) : super(key: key);
+
+  @override
+  State<welcome> createState() => _welcomeState();
+}
+
+class _welcomeState extends State<welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
