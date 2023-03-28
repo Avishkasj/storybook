8786c39928418df8052c30e6f7ef70bd6875e223
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-28 23:34:44 +0530

    side menubar

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -10,6 +10,45 @@ class _dashboardState extends State<dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0.0,
+        title: Text('Transparent App Bar'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                // Handle navigation to the home screen
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                // Handle navigation to the settings screen
+              },
+            ),
+          ],
+        ),
+      ),
+
       body: Container(
         child: Column(
           children: [
