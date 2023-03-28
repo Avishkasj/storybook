b942344e2ba93875098167354e1ce1207bcd4d72
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-29 01:50:57 +0530

    Update dashboard.dart

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -71,27 +71,46 @@ class _dashboardState extends State<dashboard> {
               ),
             ),
             child: SafeArea(
-              child: SingleChildScrollView(
-                child: Column(
-                  // crossAxisAlignment: CrossAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Column(
-                      children: [
-                        Center(
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(10, 150, 10, 0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Flexible(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(4.0),
+              child:  Transform.translate(
+                offset: Offset(0, 100),
+                child: SingleChildScrollView(
+                  child: Column(
+                    // crossAxisAlignment: CrossAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: Card(
+                                        margin: EdgeInsets.zero,
+                                        elevation: 0,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(30.0),
+                                          child: Container(
+                                            height: 200,
+                                            width: double.infinity,
+                                            child: Center(
+                                              child: Text('My Card'),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+
+
+                                  Flexible(
                                     child: Card(
-                                      margin: EdgeInsets.zero,
-                                      elevation: 0,
                                       child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(30.0),
+                                        borderRadius: BorderRadius.circular(15.0),
                                         child: Container(
                                           height: 200,
                                           width: double.infinity,
@@ -102,51 +121,51 @@ class _dashboardState extends State<dashboard> {
                                       ),
                                     ),
                                   ),
-                                ),
+
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
 
 
-                                Flexible(
-                                  child: Card(
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(15.0),
-                                      child: Container(
-                                        height: 200,
-                                        width: double.infinity,
-                                        child: Center(
-                                          child: Text('My Card'),
+
+                      //row 2
+
+                      Column(
+                        children: [
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: Card(
+                                        margin: EdgeInsets.zero,
+                                        elevation: 0,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(30.0),
+                                          child: Container(
+                                            height: 200,
+                                            width: double.infinity,
+                                            child: Center(
+                                              child: Text('My Card'),
+                                            ),
+                                          ),
                                         ),
                                       ),
                                     ),
                                   ),
-                                ),
-
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
 
 
-
-                    //row 2
-
-                    Column(
-                      children: [
-                        Center(
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Flexible(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(4.0),
+                                  Flexible(
                                     child: Card(
-                                      margin: EdgeInsets.zero,
-                                      elevation: 0,
                                       child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(30.0),
+                                        borderRadius: BorderRadius.circular(15.0),
                                         child: Container(
                                           height: 200,
                                           width: double.infinity,
@@ -157,51 +176,51 @@ class _dashboardState extends State<dashboard> {
                                       ),
                                     ),
                                   ),
-                                ),
+
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
 
 
-                                Flexible(
-                                  child: Card(
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(15.0),
-                                      child: Container(
-                                        height: 200,
-                                        width: double.infinity,
-                                        child: Center(
-                                          child: Text('My Card'),
+                      //row 3
+
+
+                      Column(
+                        children: [
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: Card(
+                                        margin: EdgeInsets.zero,
+                                        elevation: 0,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(30.0),
+                                          child: Container(
+                                            height: 200,
+                                            width: double.infinity,
+                                            child: Center(
+                                              child: Text('My Card'),
+                                            ),
+                                          ),
                                         ),
                                       ),
                                     ),
                                   ),
-                                ),
-
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
 
 
-                    //row 3
-                    //row 2
-
-                    Column(
-                      children: [
-                        Center(
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Flexible(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(4.0),
+                                  Flexible(
                                     child: Card(
-                                      margin: EdgeInsets.zero,
-                                      elevation: 0,
                                       child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(30.0),
+                                        borderRadius: BorderRadius.circular(15.0),
                                         child: Container(
                                           height: 200,
                                           width: double.infinity,
@@ -212,34 +231,127 @@ class _dashboardState extends State<dashboard> {
                                       ),
                                     ),
                                   ),
-                                ),
+
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
 
 
-                                Flexible(
-                                  child: Card(
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(15.0),
-                                      child: Container(
-                                        height: 200,
-                                        width: double.infinity,
-                                        child: Center(
-                                          child: Text('My Card'),
+
+                      //row 4
+
+                      Column(
+                        children: [
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: Card(
+                                        margin: EdgeInsets.zero,
+                                        elevation: 0,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(30.0),
+                                          child: Container(
+                                            height: 200,
+                                            width: double.infinity,
+                                            child: Center(
+                                              child: Text('My Card'),
+                                            ),
+                                          ),
                                         ),
                                       ),
                                     ),
                                   ),
-                                ),
 
-                              ],
+
+                                  Flexible(
+                                    child: Card(
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(15.0),
+                                        child: Container(
+                                          height: 200,
+                                          width: double.infinity,
+                                          child: Center(
+                                            child: Text('My Card'),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+
+                                ],
+                              ),
                             ),
-                          ),
-                        )
-                      ],
-                    ),
+                          )
+                        ],
+                      ),
 
 
-                    Column(),
-                  ],
+                      //row 5
+
+                      Column(
+                        children: [
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: Card(
+                                        margin: EdgeInsets.zero,
+                                        elevation: 0,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(30.0),
+                                          child: Container(
+                                            height: 200,
+                                            width: double.infinity,
+                                            child: Center(
+                                              child: Text('My Card'),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+
+
+                                  Flexible(
+                                    child: Card(
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(15.0),
+                                        child: Container(
+                                          height: 200,
+                                          width: double.infinity,
+                                          child: Center(
+                                            child: Text('My Card'),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+
+
+                      Column(),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -249,3 +361,4 @@ class _dashboardState extends State<dashboard> {
     );
   }
 }
+
