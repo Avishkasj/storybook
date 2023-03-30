88b184f3738cf40eade8659807ad19b1375de7d3
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-31 00:49:22 +0530

    books and audio select

--- lib/select.dart
+++ lib/select.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class select extends StatefulWidget {
   const select({Key? key}) : super(key: key);
 
@@ -10,9 +11,141 @@ class _selectState extends State<select> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Text("data"),
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/img3.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.transparent, Colors.black.withOpacity(0.8)],
+              ),
+            ),
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Text(
+                  //   "data",
+                  //   style: TextStyle(
+                  //     color: Colors.white,
+                  //   ),
+                  // ),
+
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                    child: Container(
+                        height: 180,
+                        width: 180,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Color.fromARGB(190, 134, 108, 1),
+                        ),
+                        child: Center(
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Color.fromARGB(190, 174, 108, 1),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 20),
+                              child: Column(
+                                children: [
+                                  Icon(
+                                    Icons.audiotrack_outlined,
+                                    color: Colors.white,
+                                    size: 44.0,
+                                    semanticLabel: 'Text to announce in accessibility modes',
+                                  ),
+
+                                  Text(
+                                    "Audio",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 30,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )),
+                  ),
+
+
+
+
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 130),
+                    child: Container(
+                        height: 180,
+                        width: 180,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Color.fromARGB(190, 134, 108, 1),
+                        ),
+                        child: Center(
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Color.fromARGB(190, 174, 108, 1),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 20),
+                              child: Column(
+                                children: [
+                                  Icon(
+                                    Icons.book_online_outlined,
+                                    color: Colors.white,
+                                    size: 44.0,
+                                    semanticLabel: 'Text to announce in accessibility modes',
+                                  ),
+
+                                  Text(
+                                    "Book",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 30,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30),
+                    child: OutlinedButton(
+                      child: Text("Next"),
+                      style: OutlinedButton.styleFrom(
+                        primary: Colors.white,
+                        side: BorderSide(
+                          color: Colors.green,
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
